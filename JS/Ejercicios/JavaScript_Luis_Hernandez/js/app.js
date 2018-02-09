5eca152acc1c1d6fa201f285be7e033b814f3caa
Luifo <luifo2033@gmail.com>
2018-02-09 18:01:13 -0500

    Se captura el id por tecla presionada

--- JS/Ejercicios/JavaScript_Luis_Hernandez/js/app.js
+++ JS/Ejercicios/JavaScript_Luis_Hernandez/js/app.js
@@ -1,28 +1,33 @@
+//Se crea función para capturar ID de los elementos a los que se da click, conversión de otros
+//Se crea como una función autoejegutable
+
+(function(){
+  var operation= "";
+  document.addEventListener("click", function printID(e){
+    e = e || window.event;
+    e = e.target || e.srcElement;
+
+    console.log("Clase " + e.className);
+    //Validar si donde se hace click pertenece a la calculadora
+    if (e.className=="tecla") {
+      operation  = e.id
+      console.log("ID " + e.id);
+      //Se crea efecto de presionar tecla
+      var pressKey = document.getElementById(operation)
+      pressKey.style.width="10%";
+    }
+      return e   // Se retorna el objeto para poder utilizar el ID em cualquier momento
+  })
+})()
+
+//Patrón de módulo para las operaciones matemáticas
+
 
 var pressKey = document.querySelectorAll(".tecla"); //Obtener todas las teclas
-/* pressKey[15].id // Obtener el ID*/
+
 function validarCero(){
   var display = document.getElementById("tecla").textContent;
   if (display ==0){
 
   }
-}
-/*
-pressKey[15].onclick = function(){
-    validarCero();
-}
-for (var i = 0; i < pressKey.length; i++) {
-  var display = document.getElementById("display").textContent
-  if (display==0) {
-    alert("No puedes presionar la tecla" + pressKey[i].textContent + " teniendo la calculadora en 0")
-  }pressKey[i]
-}
-*/
-/*document.getElementById("tecla").addEventListener("click",validarCero)*/
-
-//Se crea función para capturar ID de los elementos a los que se da click
-function printID(e){
-  e = e || window.event;
-  e = e.target || e.srcElement;
-  console.log("ID " + e.id);
 }
