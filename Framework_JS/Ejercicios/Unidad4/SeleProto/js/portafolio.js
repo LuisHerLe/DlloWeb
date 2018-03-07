a6c8641dfed38676b8a8c67795f52a32ed4b3a0a
Luis Hernandez <luifo2033@gmail.com>
2018-03-07 16:18:24 -0500

    07032018-2

---
+++ Framework_JS/Ejercicios/Unidad4/SeleProto/js/portafolio.js
@@ -0,0 +1,13 @@
+
+
+// 1. El título de la descripción de Santa Marta.
+$$('.img-thumbnail')[5].next().select("h4")[0];
+
+//2. El botón oculto en la barra de navegación.
+$$(".navbar-header")[0].childElements("button")[0];
+
+//3. La imágen de la descripción de Pasto.
+$$('.img-thumbnail')[11];
+
+//4 El enlace externo a la página oficial.
+$$('footer')[0].childElements()[0].childElements()[1].select('.btn')[0];
