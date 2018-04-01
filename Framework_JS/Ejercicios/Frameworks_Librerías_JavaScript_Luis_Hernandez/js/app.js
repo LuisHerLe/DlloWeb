1da5984809f41d42ce1b24588f744fb78a802c8e
Luifo <luifo2033@gmail.com>
2018-03-31 19:47:51 -0500

    Se termina contador, se agrega func endGame, se modifica estilos del DOM

--- Framework_JS/Ejercicios/Frameworks_Librerías_JavaScript_Luis_Hernandez/js/app.js
+++ Framework_JS/Ejercicios/Frameworks_Librerías_JavaScript_Luis_Hernandez/js/app.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function(){
 
+  // ********************** INICIO VARIABLES GLOBALES **********************
+
+  var countMov=0;
+
+  // ********************** FIN VARIABLES GLOBALES **********************
+
+  var start = false;
+  console.log("OOEEEEE" + start);
   // NOTE: Punto 1: Animación del título Match Game #DCFF0E
   setInterval(function(){
     $(".main-titulo").stop().animate({color:'white'},1000);
@@ -9,24 +17,38 @@ $(document).ready(function(){
 
   // NOTE: Punto 2: Se generan las imágenes de forma aleatoria
   getRowCandies();
-
-  // NOTE:Punto 6. Se reinicia contenido al dar click al botón cuando este diga "Reiniciar"
-  $(".btn-reinicio").click(function(){
-    var contButton = $(".btn-reinicio").html();
-    if (contButton == "Iniciar") {
-      $(".btn-reinicio").html("Reiniciar");
-      //$("#timer").html("AAA");
-    //  $("#timer").countdown({startTime: "00:00:02:00"});
-
-      setInterval(backTime,1000);
-
-      $("img").draggable(); // NOTE: Punto 7: Se habilita el movimiento de las fichas
-    }else {
-      location.reload();
-    }
-  })
 })
 
+// NOTE:Punto 6. Se inicia juego o se reinicia contenido al dar click al botón cuando este diga "Reiniciar"
+$(".btn-reinicio").click(function(){
+  var contButton = $(".btn-reinicio").html();
+  start = true;
+
+
+  if (contButton == "Iniciar") {
+    $(".btn-reinicio").html("Reiniciar");
+
+    if (start) {
+      // NOTE: Se llama inicia el temporizador
+      setup(start);
+    }
+
+    $( "img" ).draggable();
+    countMov=0;
+    findHorizontal()
+    //moveCandie();
+    //var options = $( "img" ).draggable( "option" );
+    //console.log(options);
+
+    //$( "img" ).draggable({
+    //revert: true
+    //}); //Regresar la posición de la imagen!!!!
+
+    // NOTE: Punto 7: Se habilita el movimiento de las fichas
+  }else {
+    location.reload();
+  }
+})
 
 // NOTE: Punto 2: Se generan las imágenes de forma aleatoria, primero se crea la estructura dentro del DOM
 function getRowCandies(){
@@ -34,11 +56,9 @@ function getRowCandies(){
   //Se recorren las columnas
   for (var i = 1; i < 8; i++) {
     var colName = ".panel-tablero .col-"+i
-    console.log("Se va a crear la estructura para la columna:" + colName);
     //Se recorren las filas
     for (var r = 1; r < 8; r++) {
       var rowNew = $("<div class=row-"+r+"><img src='' alt=''></div>")
-      console.log("Se va a crear la estructura para la fila: col-"+ r);
       rowNew.appendTo(colName);
       getImgCandies(rowNew);
     }
@@ -54,6 +74,66 @@ function getImgCandies(rowNew){
   rowNew.find("img").attr("src",newCandy);
 }
 
-function backTime(){
+// NOTE: Punto 4: Función para temporizador usando p5
+function setup(start){
+  if (start) {
 
+
+    var counter = 0;
+    var timeLeft = 120;
+
+    //Convertir a minutos + segundos
+    function convertMinutes(s){
+      var min = floor (s / 60);
+      var sec = s % 60;
+      var time = nf(min,2) + ":" + nf(sec,2);
+      if (min == 0 && sec == 0) {
+        //NOTE: Punto 4: Se termina el juego
+        endGame();
+      }
+      return time;
+
+    }
+
+    noCanvas();
+    var timer = $("#timer");
+    timer.html(convertMinutes(timeLeft - counter))
+    function countDown(){
+      counter ++;
+      timer.html(convertMinutes(timeLeft - counter))
+    }
+    setInterval(countDown, 1000);
+  }
+}
+
+function endGame(){
+  // NOTE: Punto 4: Se oculta el tablero de juego y el tiempo, adicional se agranda al 100% el ancho del panel de puntos
+  $(".time").hide(1000,"swing");
+  $(".panel-tablero").hide(1000,"swing", function(){
+    $(".panel-score").animate({width:'100%'},1500);
+  });
+
+  // NOTE: Se crea nuevo div para indicar que se acabó el Juego
+  var rowEnd = $("<div class='main-container'><p class = 'score'>End Game!</p></div>")
+  var gameName = $(".main-titulo")
+  rowEnd.appendTo(gameName);
+}
+
+function findHorizontal(){
+  var bh=0;
+  for (var i = 1; i < 8; i++) {
+    var colName = ".panel-tablero .col-"+i
+    //Se recorren las filas
+    for (var r = 1; r < 8; r++) {
+      var match1=$(".row-"+r).children("img:nth-last-child("+i+")").attr("src")
+      var match2=$(".row-"+(r+1)).children("img:nth-last-child("+i+")").attr("src")
+      var match3=$(".row-"+(r+2)).children("img:nth-last-child("+i+")").attr("src")
+	  if (match1 != null && match2 != null && match3 != null){
+		  if (match1 == match2 && match2 == match3){
+			  console.log("Hay iguales!")
+		  }
+	  }
+    }
+  }
+  return bh;
 }
